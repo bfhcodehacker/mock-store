2f6df2646fce6632e088a222e358991d24e8504e
Brad <brad.habenicht@gmail.com>
2025-09-22 23:08:06 -0400

    feat: add react-navigation and initial screens and tabs

---
+++ src/screens/Categories.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native';
+import { MainStyles } from '../styles/MainStyles';
+
+export const CategoriesScreen = () => {
+  return (
+    <View style={MainStyles.main}>
+      <Text>Categories</Text>
+    </View>
+  );
+}
---
+++ src/stacks/HomeStack.tsx
@@ -0,0 +1,21 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "../screens/HomeScreen";
+import { CategoriesScreen } from "../screens/Categories";
+import { ProductIndexScreen } from "../screens/ProductIndex";
+import { ProductDisplayScreen } from "../screens/ProductDisplay";
+
+
+const HomeStack = createNativeStackNavigator();
+
+const HomeScreenStack = () => {
+  return (
+    <HomeStack.Navigator>
+      <HomeStack.Screen name="Home" options={{headerShown: false}} component={HomeScreen} />
+      <HomeStack.Screen name="Categories" component={CategoriesScreen} />
+      <HomeStack.Screen name="ProductIndex" component={ProductIndexScreen} />
+      <HomeStack.Screen name="ProductDisplay" component={ProductDisplayScreen} />
+    </HomeStack.Navigator>
+  );
+}
+
+export default HomeScreenStack;
