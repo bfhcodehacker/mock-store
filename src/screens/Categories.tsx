d7dce79cd4ea7930d289223f10d21e966b53e218
Brad <brad.habenicht@gmail.com>
2025-09-29 18:02:36 -0400

    feat: add categories to Categories screen

--- src/screens/Categories.tsx
+++ src/screens/Categories.tsx
@@ -1,10 +1,36 @@
-import { Text, View } from 'react-native';
-import { MainStyles } from '../styles/MainStyles';
+import { ScrollView, View } from 'react-native';
+import { GradientWrapper } from '../components/GradientWrapper';
+import { useAppSelector } from '../app/hooks';
+import { FeaturedCategory } from '../components/FeaturedCategory';
+import { Category } from '../types/category';
+import { useNavigation } from '@react-navigation/native';
+import { HomeStackParamList } from '../stacks/HomeStack';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+import { styles } from '../styles/Categories';
 
 export const CategoriesScreen = () => {
+  const navigation = useNavigation<NativeStackNavigationProp<HomeStackParamList>>();
+  const categories = useAppSelector((state) => state.storeData.categories);
+
+  const onPress = (category: Category) => () => {
+    navigation.navigate('ProductIndex', { category });
+  }
+
   return (
-    <View style={MainStyles.main}>
-      <Text>Categories</Text>
-    </View>
+    <GradientWrapper>
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.container}>
+        {categories.map(category => {
+          return <FeaturedCategory
+            onPress={onPress}
+            category={category}
+            key={category.slug}
+            boxStyle={styles.boxStyle}
+          />
+        })}
+        </View>
+      </ScrollView>
+    </GradientWrapper>
   );
 }
